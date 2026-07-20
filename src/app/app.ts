77cddef6e165b35721557aad82b126ecbb9d379d
joseantunescn <joseantunescn@outlook.com>
2026-07-20 09:48:49 -0300

    implemented post

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-produtos');
+
+  private http = inject(HttpClient);
+
+  produto = new FormGroup({
+    nome: new FormControl(''),
+    preco: new FormControl(0),
+    quantidade: new FormControl(0)
+  });
+
+  cadastrarProduto() {
+    this.http.post('http://localhost:8081/api/v1/produtos', this.produto.value)
+    .subscribe((resposta) => {
+      console.log(resposta);
+    });
+  }
+
 }
