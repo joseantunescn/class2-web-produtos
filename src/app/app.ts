7451c56fd43c6f8a5faff1df184fc5b191f6c43d
flipviduani <flipviduani@hotmail.com>
2026-07-20 21:56:57 -0300

    added post and get at the front

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,17 +14,49 @@ export class App {
 
   private http = inject(HttpClient);
 
+  mensagemSucesso = signal<string>('');
+  mensagemErro = signal<string>('');
+
+  produtos = signal<any[]>([]);
+
   produto = new FormGroup({
-    nome: new FormControl(''),
-    preco: new FormControl(0),
-    quantidade: new FormControl(0)
+    nome: new FormControl('', [Validators.required]),
+    preco: new FormControl('', [Validators.required, Validators.min(0)]),
+    quantidade: new FormControl('', [Validators.required, Validators.min(0)])
   });
+
+  ngOnInit() {
+    this.consultarProdutos();
+  }
 
   cadastrarProduto() {
     this.http.post('http://localhost:8081/api/v1/produtos', this.produto.value)
-    .subscribe((resposta) => {
-      console.log(resposta);
+    .subscribe({
+      next: (response: any) => {
+        this.mensagemSucesso.set(response.mensagem);
+        this.mensagemErro.set('');
+        this.produto.reset();
+      },
+      error: (e: any) => {
+        this.mensagemErro.set(e.error.mensagem);
+        this.mensagemSucesso.set('');
+      }
     });
   }
+
+    //Função para consultar os produtos da API
+  consultarProdutos() {
+    //fazendo uma requisição HTTP GET para a api de produtos
+    this.http.get('http://localhost:8081/api/v1/produtos')
+      .subscribe({ //Aguardando o retorno da API
+        next: (dados) => { //Capturando a resposta de foi sucesso
+          this.produtos.set(dados as any[]);
+        },
+        error: (e) => { //Capturando a resposta se foi erro
+          console.log(e.error);
+        }
+      });
+  }
+
 
 }
